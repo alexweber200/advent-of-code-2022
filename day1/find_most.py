b3654b3e05be17133b69528f4658292c6efebdda
Alex Weber <alexweber200@gmail.com>
2022-12-01 14:14:11 -0500

    Add part 2 code to find top 3 elves

--- day1/find_most.py
+++ day1/find_most.py
@@ -1,3 +1,4 @@
+# Part 1: Find the elf carrying the most calories
 elves = [0]
 elf = 0
 
@@ -9,4 +10,14 @@ with open('./input', 'r') as lines:
             elf += 1
             elves.append(0)
 
-print('Elf #{} is carrying the most calories, with a total of {}'.format(elves.index(max(elves))+1, max(elves)))
+# Part 2: Find the top three elves and get the total number of calories held by all 3
+text = ['', 'second ', 'third ']
+total = 0
+
+for elf in range(0,3):
+    max_index = elves.index(max(elves))
+    total += max(elves)
+    print('Elf #{} is carrying the {}most calories, with a total of {}'.format(max_index+1, text[elf], max(elves)))
+    elves.pop(max_index)
+
+print('\nThe top 3 elves are carrying a total of {} calories'.format(total))
